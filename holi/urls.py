434f361f4eb61e64382848041df134a0283c8f5a
Name <sachin981648276@gmail.com>
2024-03-23 15:07:40 +0530

    new project

--- holi/urls.py
+++ holi/urls.py
@@ -2,12 +2,8 @@ from holi import views
 from django.urls import path
 
 urlpatterns = [
-
-
     path("",views.homepage),
     path("aboutus",views.aboutus, name="aboutus"),
-    path("aboutus",views.aboutus, name="aboutus"),
-     
-
-    
+    path("contactus",views.contactusx, name="contactusx"),
+    path("check_form",views.check_form, name="check_form")
 ]    
--- holi/views.py
+++ holi/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse 
 
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("this is homepage")
+    return render(request,"home.html")
 def aboutus(request):
     return render(request,"aboutus.html")
+def contactusx(request):
+    return render(request,"contactus.html")
+def check_form(request):
+    return render(request,"check_form.html")
 
